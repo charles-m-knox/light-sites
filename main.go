d0dd9915a8aefb58c36cedf3a721d25c6d1fac82
charles-m-knox <8022749+charles-m-knox@users.noreply.github.com>
2020-12-19 07:37:07 -0700

    fix: reset document directory on read loop; swap documents instead of clear-then-populate
    Signed-off-by: light-sites <7856443-light-sites@users.noreply.gitlab.com>

--- main.go
+++ main.go
@@ -33,18 +33,20 @@ func main() {
 			log.Print("reading directory...")
 			documentDirectoryList.Path = conf.Directories.Documents
 
+			documentDirectoryList.Files = []string{}
 			err := documentDirectoryList.WalkDirectory()
 			if err != nil {
 				log.Fatalf("failed to read directory %v: %v", conf.Directories.Documents, err.Error())
 			}
 
-			documents = []document.Document{}
+			newDocuments := []document.Document{}
 			for _, file := range documentDirectoryList.Files {
-				_, err := document.ParseDocument(&conf, &documents, &documentDirectoryList.Files, file)
+				_, err := document.ParseDocument(&conf, &newDocuments, &documentDirectoryList.Files, file)
 				if err != nil {
 					log.Printf("failed to process document %v: %v", file, err.Error())
 				}
 			}
+			documents = newDocuments
 			log.Printf("done reading directory. %v documents found. sleeping %v.", len(documents), conf.RefreshInterval)
 			time.Sleep(conf.RefreshInterval)
 		}
